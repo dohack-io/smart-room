86d4270caa516d53e9d10f4b5d7fda31eea6a0d8
rijun <richard.jung001@stud.fh-dortmund.de>
2019-03-24 02:01:35 +0100

    Added light functions

--- index.js
+++ index.js
@@ -3,26 +3,45 @@ let fs = require('fs');
 let gpio = require('gpio');
 
 let state = 0;
+gpio.pins[16].setType(gpio.INPUT);
 gpio.pins[22].setType(gpio.OUTPUT).setValue(state);
 
-fs.readFile('src/client.html', 'utf8', (err, page) => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-
-    http.createServer((req, res) => {
-        if (req.url == '/Toggle') {
-            res.end();
-    
-            state = !state;
-            gpio.pins[22].setValue(state);
-        } else if (req.url == '/') {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(page);
-        } else {
-            res.writeHead(404);
-            res.end();
+function writeStatic(path, res) {
+    console.log(path);
+    fs.readFile('/www/' + path, (err, data) => {
+        if (err) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('fs.readFile error: ' + err);
+            return;
         }
-    }).listen(80);
-});
+
+        let contentType = 'text/html';
+        if (path.substr(-4) == '.png')
+            contentType = 'image/png';
+        else if (path.substr(-3) == '.js')
+            contentType = 'text/javascript';
+        else if (path.substr(-4) == '.css')
+            contentType = 'text/css';
+
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data);
+    });
+}
+
+http.createServer((req, res) => {
+    if (req.url == '/temp') {
+        res.end();
+        gpio.pins[16].getValue(gpio.ANALOG, (err, val) => {
+            console.log(val);
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(val);
+            return;
+        })
+    } else if (req.url == '/light') {
+        res.end();
+        state = !state;
+        gpio.pins[22].setValue(state);
+    } else {
+        writeStatic(req.url == '/' ? '/index.html' : req.url, res);
+    }
+}).listen(80);
